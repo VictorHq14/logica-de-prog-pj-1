5a605ce0a9a88f8fffef31dd8f2b56e55c4a4184
VictorHq14 <victorhugo.ce0212@gmail.com>
2024-09-16 17:55:55 -0300

    Add files via upload

---
+++ aula 2 logica/atv1.py
@@ -0,0 +1,16 @@
+idade = int(input("digite uma idade aleatoria:  "))
+idade2 = int(input("digite outra idade aleatoria:  "))
+
+if idade == idade2:
+     print(f"{idade} é igual a {idade2}")
+elif idade >= idade2:
+     print(f"{idade} é maior que {idade2}")
+else:
+     print(f"{idade2} é maior que {idade}")
+
+    
+
+
+
+
+
