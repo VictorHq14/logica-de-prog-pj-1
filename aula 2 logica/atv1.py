6d2ca3c34a1df9d72acaa847859c04c6e4c3b262
VictorHq14 <victorhugo.ce0212@gmail.com>
2024-09-16 18:01:52 -0300

    Update atv1.py 0.0.2
    mudei um sinal que estava errado

--- aula 2 logica/atv1.py
+++ aula 2 logica/atv1.py
@@ -3,7 +3,7 @@ idade2 = int(input("digite outra idade aleatoria:  "))
 
 if idade == idade2:
      print(f"{idade} é igual a {idade2}")
-elif idade >= idade2:
+elif idade > idade2:
      print(f"{idade} é maior que {idade2}")
 else:
      print(f"{idade2} é maior que {idade}")
